9c9d7d37e9181235fabd70f2193eb13020db101c
Rimu Raghubanshi <rimu.raghubanshi@convergetree.com>
2021-05-02 16:44:41 +0545

    api changes for platform wise ad display

--- app/Http/Controllers/API/V2/ApiController.php
+++ app/Http/Controllers/API/V2/ApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API\V2;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Platform;
+use App\Taxonomy;
 use App\Term;
 use Carbon\Carbon;
 use App\Ad;
@@ -80,14 +81,22 @@ class ApiController extends Controller
     }
 
     public function ads(Request $request){ 
+
     	$data = [];
     	$response = ['status'=>true,'data'=>$data];
+
     	//parameters status,platform,adType,adCategory
     	//status: Running, Expired, Scheduled
     	//$validStatus = ['Running','Expired','Scheduled'];
     	$status   = 'Running';
-    	$isValidPlatform = $this->validatePlatform($request->platform);
-    	$isValid = $this->validateCategory($request->category);
+
+    	$isValidPlatform 	= $this->validatePlatform($request->platform);
+    	$isValid 			= $this->validateCategory($request->category);
+
+		if($isValid['check'] == ''){
+			
+			$isValid = $this->allCategory();			
+		}
 
         if($isValid['check'] && !$isValid['valid']){
             return response()->json($response);
@@ -96,58 +105,61 @@ class ApiController extends Controller
             return response()->json($response);
         } 
     	
-        $category = $isValid['value'];
+        $categories = $isValid['value'];	
 
-    	if(isset($category->id)){
+		if($categories){
+			foreach($categories as $category){
 
-            
-            if($category->children->count()>0){
-                foreach($category->children as $child){
-                    if($isValidPlatform['valid']){
-                         $ads = $child->runningAds->where('platform_id',$isValidPlatform['value']);
-                     }else{
-                         $ads = $child->runningAds;
-                     }
-                   
-                    if(!$ads->isEmpty()){
-                    $i=0;
-                    foreach($ads as $at){
-                        $data[$isValid['check']][$child->queryKey][$i]['platform'] = isset($at->platform->name)?$at->platform->name:'';
-                        $data[$isValid['check']][$child->queryKey][$i]['adType']= isset($at->adType->name)?$at->adType->name:'';
-                        $data[$isValid['check']][$child->queryKey][$i]['title'] = $at->title;
-                        $data[$isValid['check']][$child->queryKey][$i]['url']   = $at->url;
-                        $data[$isValid['check']][$child->queryKey][$i]['image'] = Thumbnail::url($at->image);
-                        $data[$isValid['check']][$child->queryKey][$i]['category']    = $this->Categories($at);
-                        $i++;
-                    }
-                }
-                }
-            }else{
-                if($isValidPlatform['valid']){
-                     $ads = $category->runningAds->where('platform_id',$isValidPlatform['value']); 
-                    }else{
-                        $ads = $category->runningAds;
-                     }
-                if(!$ads->isEmpty()){
-                    $i=0;
-                    foreach($ads as $at){
-                        $data[$isValid['check']][$i]['platform'] = isset($at->platform->name)?$at->platform->name:'';
-                        $data[$isValid['check']][$i]['adType']= isset($at->adType->name)?$at->adType->name:'';
-                        $data[$isValid['check']][$i]['title'] = $at->title;
-                        $data[$isValid['check']][$i]['url']   = $at->url;
-                        $data[$isValid['check']][$i]['image'] = Thumbnail::url($at->image); 
-                        $data[$isValid['check']][$i]['category']    = $this->Categories($at);
-                        $i++;
-                    }
-                }
-            } 
-            $response['data'] = $data;
-            return response()->json($response);
+				$catName = ($isValid['check'] !='') ? $isValid['check'] : $category->queryKey;
 
-            
-        }
-
-
+				if(isset($category->id)){
+				
+					if($category->children->count()>0){
+						foreach($category->children as $child){
+							if($isValidPlatform['valid']){
+								$ads = $child->runningAds->where('platform_id',$isValidPlatform['value']);
+							}else{
+								$ads = $child->runningAds;
+							}
+						
+							if(!$ads->isEmpty()){
+							$i=0;
+							foreach($ads as $at){
+								$data[$catName][$child->queryKey][$i]['platform'] 	= isset($at->platform->name)?$at->platform->name:'';
+								$data[$catName][$child->queryKey][$i]['adType']		= isset($at->adType->name)?$at->adType->name:'';
+								$data[$catName][$child->queryKey][$i]['title'] 		= $at->title;
+								$data[$catName][$child->queryKey][$i]['url']   		= $at->url;
+								$data[$catName][$child->queryKey][$i]['image'] 		= Thumbnail::url($at->image);
+								$data[$catName][$child->queryKey][$i]['category']  	= $this->Categories($at);
+								$i++;
+							}
+						}
+						}
+					}else{
+						if($isValidPlatform['valid']){
+							$ads = $category->runningAds->where('platform_id',$isValidPlatform['value']); 
+							}else{
+								$ads = $category->runningAds;
+							}
+						if(!$ads->isEmpty()){
+							$i=0;
+							foreach($ads as $at){
+								$data[$catName][$i]['platform'] = isset($at->platform->name)?$at->platform->name:'';
+								$data[$catName][$i]['adType']	= isset($at->adType->name)?$at->adType->name:'';
+								$data[$catName][$i]['title'] 	= $at->title;
+								$data[$catName][$i]['url']   	= $at->url;
+								$data[$catName][$i]['image'] 	= Thumbnail::url($at->image); 
+								$data[$catName][$i]['category'] = $this->Categories($at);
+								$i++;
+							}
+						}
+					} 					
+				}
+			}
+			
+			$response['data'] = $data;
+			return response()->json($response);
+		}		 
     }
 
     private function Categories($ad){
@@ -176,16 +188,38 @@ class ApiController extends Controller
     public function validateCategory($name){
 
     	$return = ['valid'=>false,'check'=>$name,'value'=>false];
-    	$arr = [];
-    	$cat = Term::where('name',$name)->orWhere('queryKey',$name)->first();
-    	if($cat){
-    		
-            $return['valid'] = true;
-            $return['value'] = $cat;
-    		
-            
-    	}
+    	$arr 	= [];
+
+		if($name !=''){
+			
+			$cat = Term::where('name',$name)->orWhere('queryKey',$name)->get();
+			if($cat){
+				
+				$return['valid'] = true;
+				$return['value'] = $cat;				
+			}
+		}
+    	
     	return $return;
     }
+
+	public function allCategory(){
+
+		$return = ['valid'=>false,'check'=>'','value'=>false];
+
+		$slug 		= 'ad-category';
+		$taxonomy 	= Taxonomy::where('slug',$slug)->first();
+		if($taxonomy){
+
+			$pcat = Term::where('term_id',NULL)->where('taxonomy_id',$taxonomy->id)->get();
+			if($pcat){
+				
+				$return['valid'] = true;
+				$return['value'] = $pcat;				
+			}
+		}
+		
+		return $return;
+	}
 
 }
